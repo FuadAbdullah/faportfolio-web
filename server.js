0a432b2a0cdcb5215b901bc3660066ecf4e1a579
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2021-04-02 00:29:56 +0800

    Redone server.js to handle pages better and edited some pages to contain personal details

--- server.js
+++ server.js
@@ -1,38 +1,62 @@
+// **** Server Related Configuration ****
 // Load Node Modules
 var express = require('express');
 const ejs = require('ejs');
 // Initialize Express
 var app = express();
 // Render static files
-app.use(express.static('public'));
+app.use(express.static('public', {extensions: ['html']}));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 // Port the website will run on
 app.listen(3001);
+// **************************************
 
-// **** GET Routes - display pages ****
-// Root Routes
-app.get('/', function (req, res) {
+// **** URL Parameters Variables ****
+// Render Functions
+// A function to render index.html
+var rootPage = function(req, res) {
     res.render('pages/index');
-});
-// To Home
-app.get('/index.html', function (req, res) {
-    res.render('pages/index');
-});
-// To Contact
-app.get('/contact.html', function (req, res) {
+};
+// A function to render contact.html
+var contactPage = function(req, res) {
     res.render('pages/contact');
-});
-// To Hire Me
-app.get('/hire-me.html', function (req, res) {
+};
+// A function to render hire-me.html
+var hirePage = function(req, res){
     res.render('pages/hire-me');
-});
-// To Projects Grid
-app.get('/projects-grid-cards.html', function (req, res) {
+};
+// A function to render projects-grid-cards.html
+var projectPage = function(req, res){
     res.render('pages/projects-grid-cards');
-});
-// To CV
-app.get('/cv.html', function (req, res) {
+};
+// A function to render cv.html
+var cvPage = function(req, res){
     res.render('pages/cv');
-});
-// *************************************
+}
+
+// URL Params Strings
+// An string to store URL params pointing towards index page
+var indexParams = '/home';
+// A string to store URL params pointing towards contact page
+var contactParams = '/contactme';
+// A string to store URL params pointing towards hire me page
+var hireParams = '/hireme'; 
+// A string to store URL params pointing towards project page
+var projectParams = '/projects';
+// A string to store URL params pointing towards cv page
+var cvParams = '/cv';
+// **********************************
+
+// **** Routes to Pages ****
+// Root Routes
+app.route(indexParams).get(rootPage);
+// To Contact
+app.route(contactParams).get(contactPage);
+// To Hire Me
+app.route(hireParams).get(hirePage);
+// To Projects Grid
+app.route(projectParams).get(projectPage);
+// To CV
+app.route(cvParams).get(cvPage);
+// *************************
